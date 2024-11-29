65d513b0eeb530e5c2aea82feb2ac072c7a3d64a
RivanFadlani <rivanfadlani7@gmail.com>
2024-11-29 07:57:03 +0700

    add: report.css for report.php

--- report.php
+++ report.php
@@ -5,53 +5,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="report.css">
 </head>
 
 <body>
-    <?php
-    echo $guru = $_POST['guru'];
-    echo $mapel = $_POST['mapel'];
-    echo $kkm = $_POST['kkm'];
-    $kelas = $_POST['kelas'];
-    $nis = $_POST['nis'];
-    $nama = $_POST['nama'];
-    $nilai = $_POST['nilai'];
-    if ($kelas == 1) {
-        echo "XII RPL 1";
-    } else {
-        echo "XII RPL 2";
-    }
-    ?>
+    <div class="container">
+        <div class="hero">
+            <img src="logo.png" alt="logo" width="100px">
+            <div class="kanan">
+                <h1>SEKOLAH MENENGAH ATAS</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos nobis, ea animi dolorum magni aut.</p>
+            </div>
+        </div>
 
-    <table border="1">
-        <tr>
-            <td>No</td>
-            <td>NIS</td>
-            <td>Nama</td>
-            <td>Nilai</td>
-            <td>Keterangan</td>
-        </tr>
         <?php
-        $s = count($nis);
-        $no = 1;
-        for ($i = 0; $i < $s; $i++) {
+        $nis = $_POST['nis'];
+        $nama = $_POST['nama'];
+        $nilai = $_POST['nilai'];
         ?>
+
+        <div class="info">
+            <p><strong>Nama Guru : </strong> <?= $guru = $_POST['guru']; ?></p>
+            <p><strong>Mata Pelajaran : </strong><?= $mapel = $_POST['mapel']; ?></p>
+            <p><strong>KKM : </strong><?= $kkm = $_POST['kkm']; ?></p>
+            <p><strong>Kelas : </strong><?php $kelas = $_POST['kelas'];
+                                        if ($kelas == 1) {
+                                            echo "XII RPL 1";
+                                        } else {
+                                            echo "XII RPL 2";
+                                        } ?></p>
+        </div>
+
+        <table border="0" cellpadding="10">
             <tr>
-                <td><?= $no; ?></td>
-                <td><?= $nis[$i]; ?></td>
-                <td><?= $nama[$i]; ?></td>
-                <td><?= $nilai[$i]; ?></td>
-                <td><?php if ($nilai[$i] > $kkm) {
-                        echo "Kompeten";
-                    } else {
-                        echo "Tidak Kompeten";
-                    }
-                    ?>
-                </td>
+                <th>No</th>
+                <th>NIS</th>
+                <th>Nama</th>
+                <th>Nilai</th>
+                <th>Keterangan</th>
             </tr>
-        <?php $no++;
-        } ?>
-    </table>
+            <?php
+            $s = count($nis);
+            $no = 1;
+            for ($i = 0; $i < $s; $i++) {
+            ?>
+                <tr>
+                    <td><?= $no; ?></td>
+                    <td><?= $nis[$i]; ?></td>
+                    <td><?= $nama[$i]; ?></td>
+                    <td><?= $nilai[$i]; ?></td>
+                    <td><?php if ($nilai[$i] >= $kkm) {
+                            echo "Kompeten";
+                        } else {
+                            echo "Tidak Kompeten";
+                        }
+                        ?>
+                    </td>
+                </tr>
+            <?php $no++;
+            } ?>
+        </table>
+    </div>
 </body>
 
 </html>
